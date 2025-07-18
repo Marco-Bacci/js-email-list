cfe99e6fcc28b258d65c30f58509978dfa851117
Marco Bacci <marcobacci94@gmail.com>
2025-07-18 16:37:10 +0200

    feat: add list style

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,10 @@
-const emails = document.getElementById(`emails`)
-const email10 = []
-for (let i=0; i<10; i++){
-axios.get(`https://flynn.boolean.careers/exercises/api/random/mail`).then((resp)=>{
-  email10.push(resp.data.response)
-  emails.innerHTML += `<ul><li>${email10[i]}</li></ul>`
-})
+const emails = document.getElementById(`emails`);
+const email10 = [];
+for (let i = 0; i < 10; i++) {
+  axios
+    .get(`https://flynn.boolean.careers/exercises/api/random/mail`)
+    .then((resp) => {
+      email10.push(resp.data.response);
+      emails.innerHTML += `<ul class="list-unstyled"><li>${email10[i]}</li></ul>`;
+    });
 }
