0019f4d359476bafe480b1257511417b78c1ffe8
Marco Bacci <marcobacci94@gmail.com>
2025-07-19 11:24:39 +0200

    feat: refresh mails with button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,23 @@
 const emails = document.getElementById(`emails`);
-const array = []
 
-for (let i = 0; i < 10; i++) {
-  axios
-    .get(`https://flynn.boolean.careers/exercises/api/random/mail`)
-    .then((resp) => {
-      const email = resp.data.response;
-      emails.innerHTML += `<li>${email}</li>`;
-      array.push(email)
-    });
-}
-console.log(array)
+// creo funzione che mi svolge fetch di 10 emails
+const getEmailList = () => {
+  for (let i = 0; i < 10; i++) {
+    axios
+      .get(`https://flynn.boolean.careers/exercises/api/random/mail`)
+      .then((resp) => {
+        const email = resp.data.response;
+        emails.innerHTML += `<li>${email}</li>`;
+      });
+    }
+  };
+  
+  getEmailList();
+  
+  // richiamo il refresh button dal dom
+  const refresh = document.getElementById("refresh");
+  
+  refresh.addEventListener("click", () => {
+    emails.innerHTML = getEmailList();
+  });
+  
